b08270b93dbd07dec1874f9e608b14eceb7f2133
Elena <bespalova.e.v@mail.ru>
2022-11-09 17:37:53 +0800

    counter+new array

--- TaskExam/Program.cs
+++ TaskExam/Program.cs
@@ -28,6 +28,34 @@ void PrintArray(string[] array)
     }
     Console.WriteLine("]");
 }
+int CountLessThan3Elements(string[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+            count++;
+    }
+    return count;
+}
+string[] ArrayLess3Element(string[] array, int count)
+{
+    
+    string[] counter = new string[count];
+    for (int i = 0, j = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            counter[j] = array[i];
+            j++;
+        }
+
+    }
+    return counter;
+}
 
 string[] array = CreateArray(4);
 PrintArray(array);
+int count = CountLessThan3Elements(array);
+string[] counter = ArrayLess3Element(array, count);
+PrintArray(counter);
